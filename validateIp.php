2c2d6e9d2ff648f677e98396d6129add1956ea3a
All4site <35457395+all4site@users.noreply.github.com>
2019-12-16 19:15:37 +0200

    home

--- validateIp.php
+++ validateIp.php
@@ -10,13 +10,16 @@
 <body>
 <!-- Пример для передачи правильного адреса http://localhost:3000/validateIp.php?ip=127.0.0.1-->
 <?php
-$validateIP = filter_var($_GET['ip'], FILTER_VALIDATE_IP, FILTER_FLAG_IPV4);
-$novaledateIP = htmlspecialchars($_GET['ip']);
-if (isset($validateIP) && $validateIP != (bool) false){
+$globalIP = @$_GET['ip'];
+$validateIP = @filter_var($globalIP, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4);
+$novaledateIP = @htmlspecialchars($_GET['ip']);
 
+if (isset($globalIP) & $validateIP == (bool) true){
 	echo "Вы ввели валидный IP <h1 style='color: green'>$validateIP</h1>";
-}else{
+}elseif(isset($globalIP) & $validateIP == (bool) false){
 	echo "Вы ввели <h1 style='color: red'>$novaledateIP</h1> а это НЕ валидный IP!!!";
+}else{
+	echo 'Вы не ввели данные вообще';
 }
 ?>
 </body>
